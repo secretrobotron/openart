77e25a1e7dbb6163e43dd94f1ed360eab3161b92
secretrobotron <secretrobotron@gmail.com>
2013-07-09 03:44:09 -0300

    s3 instead

--- app.js
+++ app.js
@@ -1,96 +1,106 @@
-var tumblr = require('tumblr.js');
-var habitat = require('habitat');
 var express = require('express');
+var knox = require('knox');
+var habitat = require('habitat');
 var os = require('os');
 var path = require('path');
-var passport = require('passport');
-var passportTumblr = require('passport-tumblr');
+var shortid = require('shortid');
+var nunjucks = require('nunjucks');
+var fs = require('fs');
+var Bitly = require('bitly');
 
 habitat.load();
 
 var env = new habitat();
+var s3 = env.get('S3');
+
+var express = require('express');
 var app = express();
 
-var oauthConfig = env.get('OAUTH');
-var sessionConfig = env.get('SESSION');
-var tumblrConfig = env.get('TUMBLR');
-
-var hostname = env.get('HOSTNAME') || os.hostname();
 var port = env.get('PORT');
+var hostname = env.get('HOSTNAME') || os.hostname();
+var allowedDomains = (env.get('ALLOWED_DOMAINS') || '').split(' ');
+var allowedIPs = (env.get('ALLOWED_IPS') || '').split(' ');
 
-app.use(express.cookieParser());
+var knoxClient = knox.createClient({
+  key: s3.key,
+  secret: s3.secret,
+  bucket: s3.bucket
+});
+
 app.use(express.bodyParser());
-app.use(express.session({secret: sessionConfig.secret}));
-app.use(passport.initialize());
-app.use(passport.session());
 
-function isAuthenticated (req, res, next) {
-  if (req.session.authenticated) {
-    next();
-  }
-  else {
-    res.redirect('/auth/login');
-  }
+function allowCorsRequests (req, resp, next) {
+  var origin = req.get('origin'); // TODO: Check if this is spoof-proof
+
+  /**
+   * Browsers don't support passing a single list so we have to loop
+   * and return a single domain when we detect a match
+   */
+  allowedDomains.forEach(function(el, index, array) {
+    if (origin === el) {
+      resp.header('Access-Control-Allow-Origin', el);
+    }
+  });
+  resp.header('Access-Control-Allow-Methods', 'POST');
+  // Access-Control-Allow-Headers
+  next();
+};
+
+app.use(allowCorsRequests);
+
+function sendKnoxHTMLRequest (filename, data, callback) {
+  var knoxReq = knoxClient.put(filename, {
+    'x-amz-acl': 'public-read',
+    'Content-Length': Buffer.byteLength(data, 'utf8'),
+    'Content-Type': 'text/html'
+  });
+
+  knoxReq.on('response', callback);
+
+  knoxReq.end(data);
 }
 
-app.get('/', isAuthenticated, function (req, res) {
-  res.send('serving!');
-});
+var bitlyConfig = env.get('BITLY');
+var bitly = new Bitly(bitlyConfig.user, bitlyConfig.key);
 
-app.get('/fail', function (req, res) {
-  res.send('failed to login :(');
-});
+var nunjucksEnv = new nunjucks.Environment(new nunjucks.FileSystemLoader(path.join(__dirname, '../examples')));
+nunjucksEnv.express(app);
 
-var id = 0;
+var publishUrl = 'http://' + s3.bucket + '.s3.amazonaws.com'
 
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
+app.use(express.static(path.join(__dirname, '../')));
 
-passport.deserializeUser(function (id, done) {
-  done(null, {});
-});
-
-passport.use(new passportTumblr.Strategy({
-    consumerKey: oauthConfig.consumer_key,
-    consumerSecret: oauthConfig.consumer_secret,
-    callbackURL: "http://localhost:3030/auth/tumblr/callback"
-  },
-  function(token, tokenSecret, profile, done) {
-    oauthConfig.token = token;
-    oauthConfig.token_secret = tokenSecret;
-    done(null, ++id);
+app.post('/post', function (req, res) {
+  if (allowedIPs.indexOf(req.connection.remoteAddress) === -1) {
+    res.send('No.', 500);
+    return;
   }
-));
 
-var tumblrClient;
+  var inputData = req.body.data;
 
-app.get('/auth/login', passport.authenticate('tumblr'));
-app.get('/auth/tumblr/callback', 
-  passport.authenticate('tumblr', {failureRedirect: '/auth/login'}),
-  function(req, res) {
-    req.session.authenticated = true;
+  if (!inputData) {
+    res.send('No.', 500);
+    return;
+  }
 
-    tumblrClient = tumblr.createClient({
-      consumer_key: oauthConfig.consumer_key,
-      consumer_secret: oauthConfig.consumer_secret,
-      token: oauthConfig.token,
-      token_secret: oauthConfig.token_secret
-    });
+  var filename = shortid.generate();
+  var longUrl = 'http://s3.amazonaws.com/' + s3.bucket + '/' + filename;
 
-    res.redirect('/');
-  });
-
-app.use(express.logger('dev'));
-
-app.post('/post', isAuthenticated, function (req, res) {
-  var data = req.body.data;
-  var type = req.body.type;
-  tumblrClient[type](tumblrConfig.blog, {body: data}, function (err, data) {
-    res.send('ok: ' + data.id, 200);
+  sendKnoxHTMLRequest(filename, inputData, function (knoxRes) {
+    if (200 == knoxRes.statusCode) {
+      bitly.shorten(longUrl, function(err, response) {
+        var url = (err || response.status_code !== 200) ? longUrl : response.data.url;
+        res.json({url: url}, 200);
+      });
+    }
+    else {
+      res.json({error: 'Couldn\'t save to S3'}, 500);
+    }
   });
 });
 
-app.listen(env.get('PORT'), function(){
-  console.log('Express server listening on ' + os.hostname() + ':' + env.get('PORT'));
+app.use(express.logger("dev"));
+
+var server = app.listen(port, function(){
+  console.log('Express server listening on ' + server.address().address + ':' + server.address().port);
 });
